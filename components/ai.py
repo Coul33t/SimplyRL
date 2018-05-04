1616e2525d87d54be0cc76fef196d3a2916dacf1
Coul33t <Coulis1990@gmail.com>
2018-05-04 16:56:45 +0200

    Added AI sys/comp

---
+++ components/ai.py
@@ -0,0 +1,3 @@
+class Ai(object):
+    def __init__(self, ai_type='Basic'):
+        self.ai_type = ai_type
---
+++ systems/sys_ai.py
@@ -0,0 +1,9 @@
+from components.ai import Ai
+from systems.sys_template import *
+
+class SysAi(SysTemplate):
+    def __init__(self):
+        super().__init__()
+
+    def create_component(self, entity, **params):
+        self.component_list[entity] = Ai(**params)
