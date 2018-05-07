350d5f44268c182545a2a247af2b64cb79eb1533
Coul33t <Coulis1990@gmail.com>
2018-05-07 16:27:56 +0200

    Added nam to the Graphics component

--- components/graphics.py
+++ components/graphics.py
@@ -1,6 +1,7 @@
 class Graphics(object):
-    def __init__(self, ch='X', fg=None, bg=None, always_visible=False):
+    def __init__(self, ch='X', name='NONE', fg=None, bg=None, always_visible=False):
         self.ch = ch
+        self.name = name
         self.fg = fg
         self.bg = bg
         self.always_visible = always_visible
