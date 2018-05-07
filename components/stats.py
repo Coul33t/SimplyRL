6cb3a9736cb52274bfe84a56bd4594822904acc6
Coul33t <Coulis1990@gmail.com>
2018-05-07 16:28:10 +0200

    Added the vision range to the Stats component

--- components/stats.py
+++ components/stats.py
@@ -1,8 +1,8 @@
 class Stats(object):
-	def __init__(self, hp=1, stamina=0, mana=0, defense=0, 
-				 melee_dmg=0, ranged_dmg=0, level=1, xp=0, 
-				 xp_given=0, is_dead=False):
-		
+	def __init__(self, hp=1, stamina=0, mana=0, defense=0,
+				 melee_dmg=0, ranged_dmg=0, level=1, xp=0,
+				 xp_given=0, is_dead=False, vision_range=5):
+
 		self.hp = hp
 		self.stamina = stamina
 		self.mana = mana
@@ -13,4 +13,4 @@ class Stats(object):
 		self.xp = xp
 		self.xp_given = xp_given
 		self.is_dead = is_dead
-		
+		self.vision_range = vision_range
