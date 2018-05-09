fde3e12e96136c81c02eab26d29767be70887704
Coul33t <Coulis1990@gmail.com>
2018-05-09 11:06:26 +0200

    Default colours to None

--- components/message.py
+++ components/message.py
@@ -1,5 +1,5 @@
 class Message(object):
-    def __init__(self, txt='DEFAULT', bg_colour='black', fg_colour='white'):
+    def __init__(self, txt='DEFAULT', bg_colour=None, fg_colour=None):
         self.txt = txt
         self.bg_colour = bg_colour
         self.fg_colour = fg_colour
